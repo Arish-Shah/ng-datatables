f20e7ec76954b268105b22c6e87b59a11e4ea75b
Arish-Shah <arish0246@gmail.com>
2020-06-09 12:29:59 +0530

    able to pass options

--- projects/examples/src/app/app.component.ts
+++ projects/examples/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Options } from 'ng-datatables';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'examples';
+export class AppComponent implements OnInit {
+  options: Options;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.options = {
+      baseAPIUrl: 'foo',
+    };
+  }
 }
