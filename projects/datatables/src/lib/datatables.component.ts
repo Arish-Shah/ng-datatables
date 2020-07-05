c93c352ad4c8ce0e9ea16203ab2945fb56ce7db4
Arish Shah <arish0246@gmail.com>
2020-07-06 00:38:21 +0530

    remove routes

--- projects/datatables/src/lib/datatables.component.ts
+++ projects/datatables/src/lib/datatables.component.ts
@@ -1,17 +1,61 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Options } from './models/options.model';
+import { Component, OnInit, ViewChild, Input } from '@angular/core';
 import { DatatablesService } from './datatables.service';
+import { Subscription } from 'rxjs';
+
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { Options } from './models/options.model';
 
 @Component({
   selector: 'ng-datatable',
-  template: `<router-outlet></router-outlet>`
+  templateUrl: 'datatables.component.html',
+  styleUrls: ['./datatables.component.css']
 })
 export class DatatablesComponent implements OnInit {
   @Input('options') options: Options;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+
+  dataSource = new MatTableDataSource();
+  dataSubscription: Subscription;
+  columns = [];
+  crud;
 
   constructor(private service: DatatablesService) {}
 
   ngOnInit(): void {
     this.service.init(this.options);
+    this.dataSubscription = this.service.data.subscribe((data) => {
+      this.dataSource.data = data;
+    });
+    this.columns = this.service.columns;
+    this.crud = this.service.crud;
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  getColumns() {
+    const columns = this.columns.map((column) => column.data);
+    if (this.crud.update || this.crud.delete) columns.push('actions');
+    return columns;
+  }
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim();
+    filterValue = filterValue.toLowerCase();
+    this.dataSource.filter = filterValue;
+  }
+
+  onAdd() {
+    console.log('adding');
+  }
+
+  onEdit(item) {
+    console.log(item);
+  }
+
+  onDelete(id) {
+    console.log(id);
   }
 }
--- projects/datatables/src/lib/datatables.module.ts
+++ projects/datatables/src/lib/datatables.module.ts
@@ -1,44 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
 
 import { DatatablesComponent } from './datatables.component';
-import { TableComponent } from './components/table/table.component';
 import { AddComponent } from './components/add/add.component';
 import { EditComponent } from './components/edit/edit.component';
 import { Transform } from './pipes/transform.pipe';
 
-const appRoutes: Routes = [
-  { path: '', component: TableComponent },
-  { path: 'add', component: AddComponent },
-  { path: 'edit/:id', component: EditComponent }
-];
-
 @NgModule({
-  declarations: [
-    DatatablesComponent,
-    TableComponent,
-    AddComponent,
-    EditComponent,
-    Transform
-  ],
+  declarations: [DatatablesComponent, AddComponent, EditComponent, Transform],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes),
     HttpClientModule,
     MatTableModule,
     MatPaginatorModule,
     MatSortModule,
     MatButtonModule,
-    MatInputModule,
-    MatIconModule
+    MatInputModule
   ],
   exports: [DatatablesComponent]
 })
